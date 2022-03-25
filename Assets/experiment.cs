0b5537b9a4058c5f2b65c0a4af1ba9f6c4cf1d97
a-elrawy <abdelrhman.elrawy1@gmail.com>
2022-03-25 09:19:23 +0200

    first

---
+++ Assets/experiment.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class experiment : MonoBehaviour
+{
+    public bool enter = true;
+    public bool stay = true;
+    public bool exit = true;
+    public Animator[] atoms;
+    public GameObject[] electronic;
+    public GameObject molecule;
+    public new string name;
+    void Awake()
+    {
+       
+
+         }
+
+    void Update()
+    {
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (enter)
+        {
+            Debug.Log("entered");
+        }
+    }
+    public magnet mg;
+    private float stayCount = 0.0f;
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.name == name)
+        {
+            if (stay)
+            {
+                print(stayCount);
+                if (stayCount > 2.15f)
+                {
+                    Debug.Log("staying");
+                    molecule.SetActive(true);
+                   for(int i = 0; i < atoms.Length; i++)
+                    {
+                        Debug.Log("staying");
+                        atoms[i].enabled = false;
+                        electronic[i].SetActive(false);
+                    }
+                    mg.enabled = true;
+                    stay = false;
+                    //  stayCount = stayCount - 0.25f;
+                }
+                else
+                {
+                    stayCount = stayCount + Time.deltaTime;
+                }
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (exit)
+        {
+            mg.enabled = false;
+            stay = true;
+            molecule.SetActive(false);
+            Debug.Log("exit");
+        }
+    }
+}
+
---
+++ Assets/magnet.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class magnet : MonoBehaviour
+{
+    public GameObject magnetObject;
+    public float forcefactor;
+
+    void Update()
+    {
+        this.GetComponent<Rigidbody>().AddForce((magnetObject.transform.position - transform.position) * forcefactor * Time.deltaTime);   
+    }
+}
